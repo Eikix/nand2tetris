1acbf92eb91b051f2d3569e26b79c89f62176d89
Elias Tazartes <elias.tazartes@gmail.com>
2022-10-08 15:46:24 +0200

    complete week 1 to 4

---
+++ 04/mult/Mult.asm
@@ -0,0 +1,45 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+//
+// This program only needs to handle arguments that satisfy
+// R0 >= 0, R1 >= 0, and R0*R1 < 32768.
+
+// Put your code here.
+
+// Initialize variables:
+// RAM[INDEX] = R1
+// RAM[SUM] = R0
+
+
+
+@R0
+D=M
+@R2
+M=0
+@R1
+D=M
+@INDEX
+M=D
+@10
+D;JGT
+@22
+D;JEQ
+@R0
+D=M
+@R2
+D=D+M
+M=D
+@INDEX
+M=M-1
+D=M
+@10
+D;JGT
+@22
+0;JMP
+
+
